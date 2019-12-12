e41c99cf09aaa41ef4eb4bd6ca4acbeacbe8aec4
kaposen144 <58549054+kaposen144@users.noreply.github.com>
2019-12-12 21:36:21 +0300

    1

--- src/index.js
+++ src/index.js
@@ -20,14 +20,14 @@ const useStyles = makeStyles(theme => ({
   card1: {
     minWidth: 600,
     height: 79,
-    marginBottom: 20,
-    marginRight: 50,
-    marginLeft: 50
+    marginBottom: 5,
+    marginRight: 150,
+    marginLeft: 150
   },
   card: {
     minWidth: 600,
-    marginBottom: 20,
-    marginRight: 50
+    marginBottom: 5,
+    marginRight: 150
   },
   header: {
     alignItems: "flex-start"
@@ -35,6 +35,12 @@ const useStyles = makeStyles(theme => ({
   avatar: {
     width: 49,
     height: 49
+  },
+  img: {
+    width: 500,
+    height: "auto",
+    marginTop: 10,
+    borderRadius: 10
   }
 }));
 const App = () => {
@@ -52,18 +58,7 @@ const App = () => {
     >
       <div className={classes.app}>
         <Card className={classes.card1} />
-        <Card className={classes.card}>
-          <CardHeader
-            className={classes.header}
-            avatar={
-              <Avatar
-                aria-label="avatar"
-                className={classes.avatar}
-                src="https://pbs.twimg.com/profile_images/1175295104912961537/TO5Wcyda_reasonably_small.jpg"
-              />
-            }
-          />
-        </Card>
+
         <Card className={classes.card}>
           <CardHeader
             className={classes.header}
@@ -121,39 +116,76 @@ const App = () => {
             }
           />
         </Card>
-        <Card className={classes.card}>
-          <CardHeader
-            className={classes.header}
-            avatar={
-              <Avatar
-                aria-label="avatar"
-                className={classes.avatar}
-                src="https://pbs.twimg.com/profile_images/1175295104912961537/TO5Wcyda_reasonably_small.jpg"
-              />
-            }
-            title={
-              <Typography variant="subtitle2">
-                Oxxxymiron @norimyxxxo · Nov 6
-              </Typography>
-            }
-            subheader={
-              <React.Fragment>
-                <Typography variant="body2">
-                  Наконец-то на самом заседании, а не в зале трансляции. Семья
-                  Егора провела в зал как брата ^_^, приставы все поняли и на
-                  этот раз отреагировали адекватно.
+        <div>
+          <Card className={classes.card}>
+            <CardHeader
+              className={classes.header}
+              avatar={
+                <Avatar
+                  aria-label="avatar"
+                  className={classes.avatar}
+                  src="https://pbs.twimg.com/profile_images/1175295104912961537/TO5Wcyda_reasonably_small.jpg"
+                />
+              }
+              title={
+                <Typography variant="subtitle2">
+                  Oxxxymiron @norimyxxxo · Nov 6
                 </Typography>
-                <CardActions
-                  style={{ justifyContent: "space-between", padding: 0 }}
-                >
-                  <LikeButton />
-                  <ShareIcon />
-                  <ShareIcon />
-                </CardActions>
-              </React.Fragment>
-            }
-          />
-        </Card>
+              }
+              subheader={
+                <React.Fragment>
+                  <Typography variant="body2">
+                    Наконец-то на самом заседании, а не в зале трансляции. Семья
+                    Егора провела в зал как брата ^_^, приставы все поняли и на
+                    этот раз отреагировали адекватно.
+                  </Typography>
+                  <CardActions
+                    style={{ justifyContent: "space-between", padding: 0 }}
+                  >
+                    <LikeButton />
+                    <ShareIcon />
+                    <ShareIcon />
+                  </CardActions>
+                </React.Fragment>
+              }
+            />
+          </Card>
+          <Card className={classes.card}>
+            <CardHeader
+              className={classes.header}
+              avatar={
+                <Avatar
+                  aria-label="avatar"
+                  className={classes.avatar}
+                  src="https://pbs.twimg.com/profile_images/1175295104912961537/TO5Wcyda_reasonably_small.jpg"
+                />
+              }
+              title={
+                <Typography variant="subtitle2">
+                  Oxxxymiron @norimyxxxo · Nov 6
+                </Typography>
+              }
+              subheader={
+                <React.Fragment>
+                  <Typography variant="body2">
+                    Вынесу из коментов к посту на РиПе
+                  </Typography>
+                  <img
+                    className={classes.img}
+                    src="https://pbs.twimg.com/media/EK80DebXsAAqgzQ?format=jpg&name=medium"
+                  />
+                  <CardActions
+                    style={{ justifyContent: "space-between", padding: 0 }}
+                  >
+                    <LikeButton />
+                    <ShareIcon />
+                    <ShareIcon />
+                  </CardActions>
+                </React.Fragment>
+              }
+            />
+          </Card>
+        </div>
       </div>
     </HorizontalScroll>
   );
